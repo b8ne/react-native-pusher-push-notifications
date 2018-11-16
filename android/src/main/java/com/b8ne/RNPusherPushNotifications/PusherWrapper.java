551cc2efbc2a71eb2da8b69f242b77dc8c2a5425
Mike Bridge <mike@zept.ca>
2018-11-15 17:57:19 -0700

    empty spike for push-notifications-android sdk

--- android/src/main/java/com/b8ne/RNPusherPushNotifications/PusherWrapper.java
+++ android/src/main/java/com/b8ne/RNPusherPushNotifications/PusherWrapper.java
@@ -1,7 +1,12 @@
 package com.b8ne.RNPusherPushNotifications;
 
 import android.util.Log;
+
+import android.app.Activity;
+
 import java.util.Map;
+import java.util.Set;
+
 import org.json.JSONObject;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactContext;
@@ -10,75 +15,86 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.google.firebase.messaging.RemoteMessage;
-import com.pusher.android.PusherAndroid;
+//import com.pusher.android.PusherAndroid;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.pusher.android.notifications.PushNotificationRegistration;
-import com.pusher.android.notifications.fcm.FCMPushNotificationReceivedListener;
-import com.pusher.android.notifications.interests.InterestSubscriptionChangeListener;
-import com.pusher.android.notifications.tokens.PushNotificationRegistrationListener;
+//import com.pusher.android.notifications.PushNotificationRegistration;
+//import com.pusher.android.notifications.fcm.FCMPushNotificationReceivedListener;
+//import com.pusher.android.notifications.interests.InterestSubscriptionChangeListener;
+//import com.pusher.android.notifications.tokens.PushNotificationRegistrationListener;
 
+import com.google.firebase.messaging.RemoteMessage;
+import com.pusher.pushnotifications.PushNotifications;
+import com.pusher.pushnotifications.SubscriptionsChangedListener;
+import com.pusher.pushnotifications.PushNotificationReceivedListener;
+
+//
+// TODO: verify the android manifest after https://docs.pusher.com/beams/reference/android
 /**
  * Created by bensutter on 13/1/17.
+ * https://docs.pusher.com/beams/getting-started/android/init-beams
  */
 
 public class PusherWrapper {
-    private static PushNotificationRegistration nativePusher;
+    //private static PushNotificationRegistration nativePusher;
     private String registeredEvent = "registered";
     private String notificationEvent = "notification";
 
     public PusherWrapper(String appKey) {
         Log.d("PUSHER_WRAPPER", "Creating Pusher with App Key: " + appKey);
         System.out.print("Creating Pusher with App Key: " + appKey);
-        if (nativePusher == null) {
-            PusherAndroid pusher = new PusherAndroid(appKey);
-            nativePusher = pusher.nativePusher();
-        }
+//        if (nativePusher == null) {
+//            PusherAndroid pusher = new PusherAndroid(appKey);
+//            nativePusher = pusher.nativePusher();
+//        }
     }
+
 
     public PusherWrapper(String appKey, final ReactContext context) {
       Log.d("PUSHER_WRAPPER", "Creating Pusher with App Key: " + appKey);
       System.out.print("Creating Pusher with App Key: " + appKey);
-        if (nativePusher == null) {
-            PusherAndroid pusher = new PusherAndroid(appKey);
-            nativePusher = pusher.nativePusher();
-        }
+        //if (nativePusher == null) {
+            //PusherAndroid pusher = new PusherAndroid(appKey);
+            //nativePusher = pusher.nativePusher();
+        //}
 
         try {
-            nativePusher.registerFCM(context, new PushNotificationRegistrationListener() {
-                @Override
-                public void onSuccessfulRegistration() {
-                    Log.d("PUSHER_WRAPPER", "Successfully registered to FCM");
-                    System.out.print("Successfully registered to FCM");
-                    context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(registeredEvent, null);
-                }
+            //PushNotifications.subscribe("hello");
 
-                @Override
-                public void onFailedRegistration(int statusCode, String response) {
-                    Log.d("PUSHER_WRAPPER", "FCM Registration failed with code " + statusCode + " " + response);
-                    System.out.print("FCM Registration failed with code " + statusCode + " " + response);
-                }
-            });
-            nativePusher.setFCMListener(new FCMPushNotificationReceivedListener() {
-                @Override
-                public void onMessageReceived(RemoteMessage remoteMessage) {
-                    WritableMap map = Arguments.createMap();
-                    RemoteMessage.Notification notification = remoteMessage.getNotification();
-
-                    if(notification != null) {
-                        map.putString("body", notification.getBody());
-                        map.putString("title", notification.getTitle());
-                        map.putString("tag", notification.getTag());
-                        map.putString("click_action", notification.getClickAction());
-                        map.putString("icon", notification.getIcon());
-
-                        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(notificationEvent, map);
-                        System.out.print(remoteMessage.toString());
-                    }
-                    else {
-                        Log.d("PUSHER_WRAPPER", "No notification received");
-                    }
-                }
-            });
+//            nativePusher.registerFCM(context, new PushNotificationRegistrationListener() {
+//                @Override
+//                public void onSuccessfulRegistration() {
+//                    Log.d("PUSHER_WRAPPER", "Successfully registered to FCM");
+//                    System.out.print("Successfully registered to FCM");
+//                    context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(registeredEvent, null);
+//                }
+//
+//                @Override
+//                public void onFailedRegistration(int statusCode, String response) {
+//                    Log.d("PUSHER_WRAPPER", "FCM Registration failed with code " + statusCode + " " + response);
+//                    System.out.print("FCM Registration failed with code " + statusCode + " " + response);
+//                }
+//            });
+//            nativePusher.setFCMListener(new FCMPushNotificationReceivedListener() {
+//                @Override
+//                public void onMessageReceived(RemoteMessage remoteMessage) {
+//                    WritableMap map = Arguments.createMap();
+//                    RemoteMessage.Notification notification = remoteMessage.getNotification();
+//
+//                    if(notification != null) {
+//                        map.putString("body", notification.getBody());
+//                        map.putString("title", notification.getTitle());
+//                        map.putString("tag", notification.getTag());
+//                        map.putString("click_action", notification.getClickAction());
+//                        map.putString("icon", notification.getIcon());
+//
+//                        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(notificationEvent, map);
+//                        System.out.print(remoteMessage.toString());
+//                    }
+//                    else {
+//                        Log.d("PUSHER_WRAPPER", "No notification received");
+//                    }
+//                }
+//            });
 
         } catch (Exception ex) {
             Log.d("PUSHER_WRAPPER", "Exception in PusherWrapper " + ex.getMessage());
@@ -86,45 +102,89 @@ public class PusherWrapper {
         }
     }
 
-    public PushNotificationRegistration getNativePusher() {
-        return this.nativePusher;
-    }
+//    public PushNotificationRegistration getNativePusher() {
+//        return this.nativePusher;
+//    }
 
-    public void subscribe(final String interest, final Callback errorCallback, final Callback successCallback) {
+    public void subscribe(final String interest) {
         Log.d("PUSHER_WRAPPER", "Attempting to subscribe to " +  interest);
         System.out.print("Attempting to subscribe to " +  interest);
-        nativePusher.subscribe(interest, new InterestSubscriptionChangeListener() {
-            @Override
-            public void onSubscriptionChangeSucceeded() {
-                Log.d("PUSHER_WRAPPER", "Success! " + interest);
-                System.out.print("Success! " + interest);
-                successCallback.invoke();
-            }
+        try {
+            PushNotifications.subscribe(interest);
+            Log.d("PUSHER_WRAPPER", "Success! " + interest);
+            System.out.print("Success! " + interest);
+            //successCallback.invoke();
+        } catch (Exception ex) {
+            Log.d("PUSHER_WRAPPER", "Exception in PusherWrapper " + ex.getMessage());
+            System.out.print("Exception in PusherWrapper.subscribe " + ex.getMessage());
+            // probably these should be removed
+            //errorCallback.invoke(0, ex.message);
+        }
+//        nativePusher.subscribe(interest, new InterestSubscriptionChangeListener() {
+//            @Override
+//            public void onSubscriptionChangeSucceeded() {
+//                Log.d("PUSHER_WRAPPER", "Success! " + interest);
+//                System.out.print("Success! " + interest);
+//                successCallback.invoke();
+//            }
+//
+//            @Override
+//            public void onSubscriptionChangeFailed(int statusCode, String response) {
+//                Log.d("PUSHER_WRAPPER", ":(: received " + statusCode + " with" + response);
+//                System.out.print(":(: received " + statusCode + " with" + response);
+//                errorCallback.invoke(statusCode, response);
+//            }
+//        });
+    }
 
+    public void unsubscribe(final String interest) {
+        PushNotifications.unsubscribe(interest);
+
+//        nativePusher.unsubscribe(interest, new InterestSubscriptionChangeListener() {
+//            @Override
+//            public void onSubscriptionChangeSucceeded() {
+//              Log.d("PUSHER_WRAPPER", "Success! " + interest);
+//              System.out.print("Success! " + interest);
+//              successCallback.invoke();
+//            }
+//
+//            @Override
+//            public void onSubscriptionChangeFailed(int statusCode, String response) {
+//                Log.d("PUSHER_WRAPPER", ":(: received " + statusCode + " with" + response);
+//                System.out.print(":(: received " + statusCode + " with" + response);
+//                errorCallback.invoke(statusCode, response);
+//            }
+//        });
+    }
+
+    public void unsubscribeAll() {
+        PushNotifications.unsubscribeAll();
+    }
+
+    public void getSubscriptions( final Callback subscriptionCallback) {
+        Set<String> subscriptions = PushNotifications.getSubscriptions();
+        subscriptionCallback.invoke(subscriptions);
+    }
+
+    public void setOnSubscriptionsChangedListener(final Callback subscriptionChangedListener) {
+        PushNotifications.setOnSubscriptionsChangedListener(new SubscriptionsChangedListener() {
             @Override
-            public void onSubscriptionChangeFailed(int statusCode, String response) {
-                Log.d("PUSHER_WRAPPER", ":(: received " + statusCode + " with" + response);
-                System.out.print(":(: received " + statusCode + " with" + response);
-                errorCallback.invoke(statusCode, response);
+            public void onSubscriptionsChanged(Set<String> interests) {
+                // call 
+                subscriptionChangedListener.invoke(interests);
             }
         });
     }
 
-    public void unsubscribe(final String interest, final Callback errorCallback, final Callback successCallback) {
-        nativePusher.unsubscribe(interest, new InterestSubscriptionChangeListener() {
+    public void setOnMessageReceivedListenerForVisibleActivity(final Activity activity, final Callback pushNotificationReceivedListener) {
+        PushNotifications.setOnMessageReceivedListenerForVisibleActivity(activity, new PushNotificationReceivedListener() {
             @Override
-            public void onSubscriptionChangeSucceeded() {
-              Log.d("PUSHER_WRAPPER", "Success! " + interest);
-              System.out.print("Success! " + interest);
-              successCallback.invoke();
-            }
-
-            @Override
-            public void onSubscriptionChangeFailed(int statusCode, String response) {
-                Log.d("PUSHER_WRAPPER", ":(: received " + statusCode + " with" + response);
-                System.out.print(":(: received " + statusCode + " with" + response);
-                errorCallback.invoke(statusCode, response);
+            public void onMessageReceived(RemoteMessage remoteMessage) {
+                pushNotificationReceivedListener.invoke(remoteMessage);
             }
         });
     }
+
+
+
 }
--- android/src/main/java/com/b8ne/RNPusherPushNotifications/RNPusherPushNotificationsModule.java
+++ android/src/main/java/com/b8ne/RNPusherPushNotifications/RNPusherPushNotificationsModule.java
@@ -8,6 +8,9 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+
+// SEE: https://docs.pusher.com/beams/reference/android
+
 public class RNPusherPushNotificationsModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
@@ -28,23 +31,85 @@ public class RNPusherPushNotificationsModule extends ReactContextBaseJavaModule 
     this.pusher = new PusherWrapper(appKey, this.reactContext);
   }
 
+//    @ReactMethod
+//    public void subscribe(final String channel, final Callback errorCallback, final Callback successCallback) {
+//        AsyncTask.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                pusher.subscribe(channel, errorCallback, successCallback);
+//            }
+//        });
+//    }
+//
+//    @ReactMethod
+//    public void unsubscribe(final String channel, final Callback errorCallback, final Callback successCallback) {
+//        AsyncTask.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                pusher.unsubscribe(channel, errorCallback, successCallback);
+//            }
+//        });
+//    }
+
     @ReactMethod
-    public void subscribe(final String channel, final Callback errorCallback, final Callback successCallback) {
+    public void subscribe(final String interest) {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                pusher.subscribe(channel, errorCallback, successCallback);
+                pusher.subscribe(interest);
             }
         });
     }
 
     @ReactMethod
-    public void unsubscribe(final String channel, final Callback errorCallback, final Callback successCallback) {
+    public void unsubscribe(final String interest) {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                pusher.unsubscribe(channel, errorCallback, successCallback);
+                pusher.unsubscribe(interest);
             }
         });
     }
+
+    @ReactMethod
+    public void unsubscribeAll() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                pusher.unsubscribeAll();
+            }
+        });
+    }
+
+    @ReactMethod
+    public void getSubscriptions( final Callback subscriptionCallback) {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                pusher.getSubscriptions(subscriptionCallback);
+            }
+        });
+    }
+
+    @ReactMethod
+    public void setOnSubscriptionsChangedListener(final Callback subscriptionChangedListener) {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                pusher.setOnSubscriptionsChangedListener(subscriptionChangedListener);
+            }
+        });
+    }
+
+    @ReactMethod
+    public void setOnMessageReceivedListenerForVisibleActivity(final Callback pushNotificationReceivedListener) {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // TODO: Figure out how to get the activity.
+                pusher.setOnMessageReceivedListenerForVisibleActivity(null, pushNotificationReceivedListener);
+            }
+        });
+    }
+
 }
